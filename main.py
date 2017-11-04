7adc8ea66cb9c34447403143b322b6affcdf8fbd
Lukáš Jagoš <lukejagos@gmail.com>
2017-11-04 23:21:34 +0100

    uprava kodu

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from collections import Counter
 import re
 import cmath
 
-def printData(dictionary):
+def printComposer(dictionary):
     for k, v in dictionary.items():
-        print('Composer:{}, Count:{}'.format(k, v))
+        if(k is not None):
+            print('{}:{}'.format(k, v))
+
+def printCentury(dictionary):
+    for k, v in dictionary.items():
+        if(k is not None):
+            print('{}th century:{}'.format(k, v))
 
 def convertToInt(s):
     try:
@@ -14,31 +20,35 @@ def convertToInt(s):
     except (TypeError, ValueError):
         return None
 
+def modifyName(name):
+    pass
+
 authorCompositionCount = Counter()
 centuryCompositionCount = Counter()
 keyCompositionCount = {}
 
 f = open('scorelib.txt', encoding='utf8')
 for line in f:
-    r = re.compile(r"Composer: (.*)")
+    r = re.compile(r"Composer: ([A-Za-z]+), ([A-Za-z]+) ([A-Za-z]+)")
     m = r.match(line)
 
     if(m is not None):
-        authorCompositionCount[m.group(1)] += 1
+        name = "{}, {}. {}.".format(m.group(1),m.group(2)[0],m.group(3)[0])
+        authorCompositionCount[name] += 1
 
 
     r = re.compile(r"Composition Year: (.*)")
     m = r.match(line)
-    century = convertToInt(m)
-    if(century is not None):
-        print(century)
+    if(m is not None):
+        century = convertToInt(m.group(1))
         centuryCompositionCount[century] += 1
 
 authorDict = dict(authorCompositionCount)
 centuryDict = dict(centuryCompositionCount)
 
-'''printData(authorDict)'''
-printData(centuryDict)
+printComposer(authorDict)
+print()
+printCentury(centuryDict)
 
 
 
