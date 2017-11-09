cca8bf55dd76cf679625bb46fedad3738333d75f
Lukáš Jagoš <lukejagos@gmail.com>
2017-11-09 14:58:50 +0100

    getprint.py

---
+++ getprint.py
@@ -0,0 +1,23 @@
+from sys import argv
+import sqlite3
+import json
+
+printNumber = argv[1]
+conn = sqlite3.connect( 'scorelib.dat' )
+cursor = conn.cursor()
+
+result = cursor.execute("select score, name from edition where year = 2004")
+
+result = cursor.execute("select person.name from person join score_author on person.id = score_author.composer "
+                        "join edition on score_author.score = edition.score "
+                        "join print on edition.id = print.edition where print.id = {}".format(printNumber))
+
+authors = []
+d = {}
+for r in result:
+    authors.append(r[0])
+
+d['composers'] = authors
+chain = json.dumps(d)
+print(chain)
+conn.commit()
